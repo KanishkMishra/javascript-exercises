1bf98fc42d1ec9eb5a64998afa5b5f5ca1820ae2
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-26 13:56:18 -0400

    COmpleted removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
+    const array = arguments[0]
 
-};
+    for (let j = 1; j < arguments.length; j++) {
+        for( let i=0; i<array.length; i++) {
+            if (array[i]===arguments[j]) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
 
+    return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
