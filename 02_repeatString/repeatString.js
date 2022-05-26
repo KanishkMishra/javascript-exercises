f59d2b2cf1e0307cde1d401dfc923842c1df0b28
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-26 13:07:47 -0400

    Completed helloWorld and repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(String, num) {
+    let repeats = String;
 
+    for (i=1; i < num; i++) {
+        repeats=repeats+String;
+    }
+
+    return repeats;
 };
 
 // Do not edit below this line
