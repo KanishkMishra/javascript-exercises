04136509e85b9f6b676106a0d6e27d4b82733107
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-28 16:37:34 -0400

    Compled finde the Oldest, finds eldes individual using array.reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce(function(p1, p2) {
+        if(p1.yearOfDeath===undefined) p1.yearOfDeath = new Date().getFullYear();
+        if(p2.yearOfDeath===undefined) p2.yearOfDeath = new Date().getFullYear();
 
+        if ((p1.yearOfDeath-p1.yearOfBirth) < (p2.yearOfDeath-p2.yearOfBirth))
+            return p2;
+        else 
+            return p1;
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
