55a755028dd80a69fbb0763f1f6e64928fe38e69
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-28 16:02:42 -0400

    Completed fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let t = 1;
+    let f1 = 1;
+    let f2 = 0;
+    for (let i = index; i > 1; i--) {
+        t = f1;
 
+        f1 = f1+f2;
+
+        f2 = t;
+    }
+    return f1;
 };
 
 // Do not edit below this line
