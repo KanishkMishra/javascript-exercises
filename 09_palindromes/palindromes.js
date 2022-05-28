1d6300b3d35b0f0e1b5c4c60f435162db637946b
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-28 15:50:02 -0400

    Completed palindrome check

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const original = string.replace(/[^a-z0-9]/gi,"").toLowerCase();
+
+    const reverse = original.split("").reverse().join("");
+
+    return (original===reverse);
 
 };
 
